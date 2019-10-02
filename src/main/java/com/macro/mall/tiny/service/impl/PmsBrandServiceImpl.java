debc59892cd76b6d35d5e86a52f9a2493d63427d
stormlong <1205534401@qq.com>
2019-10-02 14:00:02 +0800

    整合Swagger-UI

--- src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
+++ src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.macro.mall.tiny.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.macro.mall.tiny.mbg.mapper.PmsBrandMapper;
 import com.macro.mall.tiny.mbg.model.PmsBrand;
 import com.macro.mall.tiny.mbg.model.PmsBrandExample;
@@ -24,5 +25,32 @@ public class PmsBrandServiceImpl implements PmsBrandService{
     @Override
     public List<PmsBrand> listAllBrand() {
         return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int createBrand(PmsBrand brand) {
+        return brandMapper.insertSelective(brand);
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand brand) {
+        brand.setId(id);
+        return brandMapper.updateByPrimaryKeySelective(brand);
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return brandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
     }
 }
